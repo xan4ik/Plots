44f888ae72512c38ad69afe49da361968242182f
Kozhevnikov Evgenii <kozhevnikov.evgenii.a@gmail.com>
2021-02-16 17:26:17 +0300

    comit that will be merged

--- Class1.cs
+++ Class1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Plots.Exeptions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,67 +25,234 @@ namespace Plots
 
     }
 
-    public abstract class DrawingCpmponent<T> : PlotComponent
+    public enum DrawQueue 
     {
-        private IModelCreator<T> modelCreator;
-        private IViewer<T> viewer;
+        Begin,
+        Process,
+        End
+    }
 
-        protected DrawingCpmponent(IModelCreator<T> modelCreator, IViewer<T> viewer)
-        {
-            this.modelCreator = modelCreator;
-            this.viewer = viewer;
-        }
+    public interface IDrawingProcessBlock 
+    {
+        void Draw(IDrawer drawer, PlotToModelProjector projector);
+        DrawQueue DrawQueue { get; }
+    }
 
-        public void Draw(IDrawer drawer, PlotToModelProjector projector) 
+    public class ProcessBlock<T> : IDrawingProcessBlock
+    {
+        public DrawQueue DrawQueue => throw new NotImplementedException();
+
+        public void Draw(IDrawer drawer, PlotToModelProjector projector)
         {
-            var model = modelCreator.CreateModel(projector);
-            viewer.ShowModel(drawer, model);
+            throw new NotImplementedException();
         }
     }
 
-    public class AxisDrawer : DrawingCpmponent<AxisMathModel>
-    {
-        public AxisDrawer(IModelCreator<AxisMathModel> modelCreator, IViewer<AxisMathModel> viewer) : base(modelCreator, viewer)
-        {
-        }
-    }
 
-    public class AxisModelViewer : IViewer<AxisMathModel>
-    {
-        public void ShowModel(IDrawer drawer, AxisMathModel model)
-        {
-            drawer.DrawLine(model.X_Begin, model.X_End);
-            drawer.DrawLine(model.Y_Begin, model.Y_End);
-        }
-    }
+    //public class PointDrawer 
+    //{
+    //    private List<PlotPoint> plotPoints;
 
-    public sealed class AxisModelCreator : IModelCreator<AxisMathModel>
-    {
-        public AxisMathModel CreateModel(PlotToModelProjector projector)
-        {
-            var modelCenter = projector.ProjectPLotPointToModel(new PlotPoint(0, 0));
-            var maxModelPoint = projector.GetMaxModelPoint(); 
+    //    public PointDrawer()
+    //    {
+    //        plotPoints = new List<PlotPoint>();
+    //    }
 
-            var xBegin = new PlotPoint(0, modelCenter.Y);
-            var yBegin = new PlotPoint(modelCenter.Y, 0);
-            var xEnd = new PlotPoint(maxModelPoint.X, modelCenter.Y);
-            var yEnd = new PlotPoint(modelCenter.X, maxModelPoint.Y);
-            
-            return new AxisMathModel(xBegin, xEnd, yBegin, yEnd);
-        }
-    }
+    //    public void Draw(IDrawer drawer, PlotToModelProjector projector)
+    //    {
+    //        var model = modelCreator.CreateModel(projector);
+    //        viewer.ShowModel(drawer, model);
+    //    }
 
-    public class PlotToModelProjector 
+
+    //    private object CreateModelValues(PlotToModelProjector projector) 
+    //    {
+
+
+    //    }
+    //}
+
+
+    //    public abstract class DrawingCpmponent<Creator, Model> : PlotComponent
+    //       where: Creator
+    //    {
+    //    private IModelCreator<T> modelCreator;
+    //    private IViewer<T> viewer;
+
+    //    protected DrawingCpmponent(IModelCreator<T> modelCreator, IViewer<T> viewer)
+    //    {
+    //        this.modelCreator = modelCreator;
+    //        this.viewer = viewer;
+    //    }
+
+    //    public void Draw(IDrawer drawer, PlotToModelProjector projector)
+    //    {
+    //        var model = modelCreator.CreateModel(projector);
+    //        viewer.ShowModel(drawer, model);
+    //    }
+    //}
+
+    //public abstract class DrawingCpmponent< T> : PlotComponent
+    //{
+    //    private IModelCreator<T> creator;
+    //    private IViewer<T> viewer;
+
+    //    protected DrawingCpmponent(IModelCreator<T> creator, IViewer<T> viewer)
+    //    {
+    //        this.creator = creator;
+    //        this.viewer = viewer;
+    //    }
+
+    //    public void Draw(IDrawer drawer, PlotToModelProjector projector)
+    //    {
+    //        var model = creator.CreateModel(projector);
+    //        viewer.ShowModel(drawer, model);
+    //    }
+    //}
+
+    //public class MathFunctionCreator : IModelCreator<MathFunctionModel>
+    //{
+
+
+
+
+    //    public MathFunctionModel CreateModel(PlotToModelProjector projector)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+
+    //public class MathFunctionModel
+    //{
+    //}
+
+
+    //public struct Range
+    //{
+    //    private int minX;
+    //    private int maxX;
+
+    //    public Range(int minX, int maxX)
+    //    {
+    //        if (maxX <= minX)
+    //            throw new DimensionException("Max X can't be lower or equal to min X");
+
+    //        this.minX = minX;
+    //        this.maxX = maxX;
+    //    }
+
+    //    public int MinX => minX;
+    //    public int MaxX => maxX;
+    //}
+    //public interface IMathFunction
+    //{
+    //    Range Range { get; }
+    //    float CountFunctionOfX(float X);
+    //}
+
+
+
+
+
+
+
+
+
+
+
+
+    //public class AxisDrawer : DrawingCpmponent<AxisMathModel>
+    //{
+    //    private int someData;
+    //    public AxisDrawer() : base(new AxisModelCreator(), new AxisModelViewer())
+    //    {
+    //    }
+    //}
+
+    //public class AxisModelViewer : IViewer<AxisMathModel>
+    //{
+    //    public void ShowModel(IDrawer drawer, AxisMathModel model)
+    //    {
+    //        drawer.DrawLine(model.X_Begin, model.X_End);
+    //        drawer.DrawLine(model.Y_Begin, model.Y_End);
+    //    }
+    //}
+
+    //public sealed class AxisModelCreator : IModelCreator<AxisMathModel>
+    //{
+    //    public AxisMathModel CreateModel(PlotToModelProjector projector)
+    //    {
+    //        var modelCenter = projector.ProjectPLotPointToModel(new PlotPoint(0, 0));
+    //        var maxModelPoint = projector.GetMaxModelPoint();
+
+    //        var xBegin = new PlotPoint(0, modelCenter.Y);
+    //        var yBegin = new PlotPoint(modelCenter.Y, 0);
+    //        var xEnd = new PlotPoint(maxModelPoint.X, modelCenter.Y);
+    //        var yEnd = new PlotPoint(modelCenter.X, maxModelPoint.Y);
+
+    //        return new AxisMathModel(xBegin, xEnd, yBegin, yEnd);
+    //    }
+    //}
+
+    //public struct AxisMathModel
+    //{
+    //    public AxisMathModel(PlotPoint x_Begin, PlotPoint x_End, PlotPoint y_Begin, PlotPoint y_End)
+    //    {
+    //        this.X_Begin = x_Begin;
+    //        this.X_End = x_End;
+    //        this.Y_Begin = y_Begin;
+    //        this.Y_End = y_End;
+    //    }
+
+    //    public PlotPoint X_Begin { get; private set; }
+    //    public PlotPoint X_End { get; private set; }
+    //    public PlotPoint Y_Begin { get; private set; }
+    //    public PlotPoint Y_End { get; private set; }
+    //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public class PlotToModelProjector
     {
         private PlotExtristics extristics;
 
-        public PlotToModelProjector( PlotExtristics extristics)
+        public PlotToModelProjector(PlotExtristics extristics)
         {
             this.extristics = extristics;
         }
 
         //TODO: Maybe intristics will do that?
-        public PlotPoint ProjectPLotPointToModel(PlotPoint plotPoint) 
+        public PlotPoint ProjectPLotPointToModel(PlotPoint plotPoint)
         {
             return new PlotPoint()
             {
@@ -93,7 +261,7 @@ namespace Plots
             };
         }
 
-        public PlotPoint ProjectModelToPlotPoint(PlotPoint model) 
+        public PlotPoint ProjectModelToPlotPoint(PlotPoint model)
         {
             return new PlotPoint()
             {
@@ -102,7 +270,7 @@ namespace Plots
             };
         }
 
-        public PlotPoint GetMaxModelPoint() 
+        public PlotPoint GetMaxModelPoint()
         {
             var frameSize = extristics.FrameSize;
             return new PlotPoint()
@@ -114,19 +282,5 @@ namespace Plots
     }
 
 
-    public struct AxisMathModel
-    {
-        public AxisMathModel(PlotPoint x_Begin, PlotPoint x_End, PlotPoint y_Begin, PlotPoint y_End)
-        {
-            this.X_Begin = x_Begin;
-            this.X_End = x_End;
-            this.Y_Begin = y_Begin;
-            this.Y_End = y_End;
-        }
 
-        public PlotPoint X_Begin { get; private set; }
-        public PlotPoint X_End { get; private set; }
-        public PlotPoint Y_Begin { get; private set; }
-        public PlotPoint Y_End { get; private set; }
-    }
 }
---
+++ PlotDecorator.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Drawing;
+
+namespace Plots_2.Decorators
+{
+    //[RepeatableComponent(true)]
+    //public partial class PlotDecorator : DrawerDecorator
+    //{
+    //    private IMathFunction function;
+
+    //    public PlotDecorator(IMathFunction function) : base(DrawingState.Process)
+    //    {
+    //        this.function = function;
+    //    }
+
+    //    public Color PlotColor { get; set; } = Color.Yellow;
+
+    //    public IMathFunction MathFunction
+    //    {
+    //        get => function;
+    //        set
+    //        {
+    //            if (value != null)
+    //            {
+    //                function = value;
+    //            }
+    //            else throw new ArgumentException("You tried set a Null function");
+    //        }
+    //    }
+
+    //    public override void Draw(Graphics graphics)
+    //    {
+    //        var newDimension = RecountDimesion(Provider.Dimension, function.Range);
+
+    //        DrawPlot(graphics, newDimension, Provider.Settings, Provider.Variables);
+    //    }
+
+    //    private void DrawPlot(Graphics graphics, DrawerDimension dimension, DrawerSettings settings, DrawerVariables variables)
+    //    {
+    //        if (function == null) throw new NullReferenceException("Don't set function");
+
+    //        using (Pen pen = new Pen(PlotColor))
+    //        {
+    //            PointF prev;
+    //            PointF next = Drawer.PointToPLotPoint(new PointF(dimension.MinX, MathFunction.CountFunctionOfX(dimension.MinX)), Provider);
+
+    //            for (float i = dimension.MinX + variables.StepX; i <= dimension.MaxX + variables.StepX; i += variables.StepX)
+    //            {
+    //                prev = next;
+    //                next = Drawer.PointToPLotPoint(new PointF(i, MathFunction.CountFunctionOfX(i)), Provider); // был + вместо -, нужно отнимать так как верхняя левая точка формы это (0, 0)
+
+    //                graphics.DrawLine(pen, prev, next);
+    //            }
+    //        }
+    //    } //метод отрисовки графика
+
+    //    private DrawerDimension RecountDimesion(DrawerDimension dimension, Range range) 
+    //    {
+    //        dimension.MaxX = Math.Min(dimension.MaxX, range.MaxX);
+    //        dimension.MinX = Math.Max(dimension.MinX, range.MinX);
+
+    //        return dimension;
+    //    }
+    //}
+
+    public partial class PlotDecorator 
+    {
+        public struct Range
+        {
+            private int minX;
+            private int maxX;
+
+            public Range(int minX, int maxX)
+            {
+                if (maxX <= minX)
+                    throw new DimensionException("Max X can't be lower or equal to min X");
+
+                this.minX = minX;
+                this.maxX = maxX;
+            }
+
+            public int MinX => minX;
+            public int MaxX => maxX;
+        }
+        public interface IMathFunction
+        {
+            Range Range { get; }
+            float CountFunctionOfX(float X);
+        }
+    }
+}
